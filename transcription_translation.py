c38a0253d6544619936fd7ce9b2f744db66bfefa
Salvador <s.tarrio@udc.es>
2024-02-20 14:25:23 +0100

    eliminar whisper

--- transcription_translation.py
+++ transcription_translation.py
@@ -1,26 +1,18 @@
-import torch, whisper,os,deepl_tr,deepgram_tr,silero,whisper_tr
+import torch, whisper,os,deepl_tr,deepgram_tr,silero
 from tqdm import tqdm
 
 
 def main(audio_path,language):
 
     transcription=True
-    transcription_mode=2 # mode 0 whisper local, mode 2 deepgram 
-    device="cuda" #only usefull on mode 0
+    transcription_mode=2 # mode 2 deepgram 
+
     translation=True
     ####deepgram models
     model_size="nova-2"
     #model_size="base"
     #model_size="enhaced"
 
-    ####whisper models
-    #model_size = "large-v2"  # @param ["medium", "large"]
-    
-    #model_size="small"
-    #model_size="medium"
-
-    #model_size="base"
-    #model_size="tiny"
 
     # @markdown Advanced settings:
     vad_threshold = 0.2  # @param {type:"number"} umbral de decision si hay audio
@@ -42,24 +34,11 @@ def main(audio_path,language):
     audio_nombre=os.path.splitext(os.path.basename(audio_path))[0]
 
     if(transcription):
-        if(device=="cuda"):
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        elif(device=="cpu"):
-            device= torch.device(device)
-        else:
-            raise Exception("No device available for ",device," in whisper. Try to install the correct torch version: https://pytorch.org/")
-        
-        if(transcription_mode==0):
-            model = whisper.load_model(model_size,device=device)
         u=silero.silero_vad(audio_path,vad_threshold,chunk_threshold)#u es la lista de chunks procesados por silero
-
         subs=[]
 
         try:
-            if(transcription_mode==0):
-                subs=whisper_tr.whisper_tr(u,model,max_attempts,language,audio_nombre)
-                model_size=".whisper_local_"+model_size
-            elif(transcription_mode==2):
+            if(transcription_mode==2):
                 subs=deepgram_tr.deepgram_tr(u,model_size,audio_nombre,language)
                 model_size=".deepgram_"+model_size
 
@@ -77,10 +56,6 @@ def main(audio_path,language):
                         os.unlink(file_path)
                 except Exception as e:
                     print(f"No se pudo eliminar {file_path}: {e}")
-        
-
-        
-        
 
 if __name__ == '__main__':
     main()
