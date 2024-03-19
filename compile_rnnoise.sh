86c77cb57aba554e13a8499d6020deecb1ef70d1
Salvador <s.tarrio@udc.es>
2024-03-19 13:23:14 +0100

    update readme

--- compile_rnnoise.sh
+++ compile_rnnoise.sh
@@ -6,3 +6,7 @@ chmod +x autogen.sh
 ./configure
 make
 make install
+python3 -m pip install torch torchaudio --index-url https://download.pytorch.org/whl/cpu
+#** Para instalar torch en tu sistema mira esta pagina:
+# https://pytorch.org/ El siguiente comando es para linux y si usas otro sistema debes cambiarlo por el que indique la web.
+python3 -m pip install -r requirements.txt
