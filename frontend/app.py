9564cb9283860a42f7952d0b350874311152e88d
Salvador <s.tarrio@udc.es>
2024-03-28 16:34:30 +0100

    frontend cambios baarra progreso

--- frontend/app.py
+++ frontend/app.py
@@ -1,31 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect, url_for, send_file,jsonify
 import os, requests
 from werkzeug.utils import secure_filename
 import zipfile
-# Ruta para la subida de archivos
-from flask import Response, stream_with_context
+
 
 # Configuración de la aplicación
 app = Flask(__name__)
 
 #docker url
-url_base='http://backend:5001'
+#url_base='http://backend:5001'
 #local url
-#url_base='http://localhost:5001'
+url_base='http://localhost:5001'
 # Configuración de la subida de archivos
+
 UPLOAD_FOLDER = 'uploads'
 DOWNLOAD_FOLDER = 'downloads'
+
 ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mkv', 'mov', 'm4v', 'mts', 'wmv', 'mpg', 'flv'}
-app.config['MAX_CONTENT_LENGTH'] = 2048 * 1024 * 1024  # Permitimos 2 GB
+
+app.config['MAX_CONTENT_LENGTH'] = 2048 * 1024 * 1024  # Permitimos hasta 2 GB
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 os.makedirs(app.config['DOWNLOAD_FOLDER'], exist_ok=True)
+
+processing_progress=0
 
 # Función para verificar la extensión del archivo
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/progress',methods=['GET'])
+def prog_var():
+    global processing_progress
+    return jsonify({'progress': processing_progress})
 
 # Ruta para la página de inicio
 @app.route('/')
@@ -38,62 +49,83 @@ def index():
     # Renderizar la página de inicio con los códigos de idioma
     return render_template('index.html', language_codes=language_codes)
 
-
-'''@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_files():
     try:
-        # Obtener los archivos y el idioma del formulario
         uploaded_files = request.files.getlist('file')
         lan = request.form.get('language')
 
-        # Variables para el progreso y la lista de subtítulos
-        total_files = len(uploaded_files)
-        uploaded_count = 0
-        subs = []
+        # Guardar archivos y obtener los subtítulos procesados
+        subs = process_files(uploaded_files, lan)
 
-        # Generar eventos SSE para informar sobre el progreso
-        def generate(file):
-            nonlocal uploaded_count
-            if file and allowed_file(file.filename):
-                # Guardar el archivo
-                filename = secure_filename(file.filename)
-                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                file.save(filepath)
-                uploaded_count += 1
+        # Generar archivo ZIP con los subtítulos
+        zip_filename = generate_zip(uploaded_files, subs)
 
-                # Actualizar el progreso de la carga
-                progress = int((uploaded_count / total_files) * 100)
+        # Generar la URL de descarga
+        zip_url = url_for('download_zip', filename=zip_filename)
 
-                # Enviar el progreso a través de SSE
-                yield f"""data: {{"fileName": "{filename}", "progress": {progress}}}"""
-
-
-                # Enviar el archivo al backend
-                backend_url = url_base + '/process_video'
-                files = {'file': (filename, open(filepath, 'rb').read(), file.mimetype)}
-                data = {'language': lan}
-                response = requests.post(backend_url, data=data, files=files)
-
-                # Obtener el subtítulo del backend
-                subtitle = response.json()
-
-                # Agregar el subtítulo a la lista
-                subs.append(subtitle)
-
-                # Eliminar el archivo temporal
-                os.remove(filepath)
-
-        # Retornar la respuesta con SSE
-        return Response(stream_with_context(generate(file) for file in uploaded_files), content_type='text/event-stream')
+        # Retornar la URL de descarga
+        return zip_url
 
     except requests.exceptions.RequestException as e:
-        # Manejar errores de comunicación con el backend
         return render_template('error.html', error_message=f"Error procesando archivos: {str(e)}"), 500
     except Exception as e:
-        # Manejar otros errores inesperados
-        return render_template('error.html', error_message=f"An unexpected error occurred: {str(e)}"), 500'''
+        return render_template('error.html', error_message=f"An unexpected error occurred: {str(e)}"), 500
 
-@app.route('/upload', methods=['POST'])
+
+def process_files(uploaded_files, lan):
+    subs = []
+    
+    global processing_progress
+    processing_progress=0
+    long=len(uploaded_files)
+    cont=0
+    for file in uploaded_files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+
+            # Enviar archivo al backend y obtener subtítulo
+            subtitle = send_to_backend(filepath, filename, lan,file.mimetype)
+
+            subs.append(subtitle)
+            os.remove(filepath)
+            cont+=1
+            processing_progress=round((cont/long)*100,1)
+    return subs
+
+def send_to_backend(filepath, filename, lan,mimetype):
+    backend_url = url_base + '/process_video'
+    files = {'file': (filename, open(filepath, 'rb').read(), mimetype)}
+    data = {'language': lan}
+    response = requests.post(backend_url, data=data, files=files)
+   
+    return response.json()
+
+
+def generate_zip(uploaded_files, subs):
+    zip_filename = 'subtitles.zip'
+    extension = ".es.srt"
+    with zipfile.ZipFile(os.path.join(app.config['DOWNLOAD_FOLDER'], zip_filename), 'w') as zipf:
+        for i, subtitle in enumerate(subs, start=1):
+            video_name = os.path.splitext(uploaded_files[i - 1].filename)[0]
+            subtitle_filename = f'{video_name}{extension}'
+            zipf.writestr(subtitle_filename, subtitle)
+    return zip_filename
+
+                               
+@app.route('/download/<filename>')
+def download_zip(filename):
+    return send_file(os.path.join(app.config['DOWNLOAD_FOLDER'], filename), as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000,debug=False)
+
+
+
+
+'''@app.route('/upload', methods=['POST'])
 def upload_files():
     try:
         # Obtener los archivos y el idioma del formulario
@@ -151,11 +183,4 @@ def upload_files():
         return render_template('error.html', error_message=f"Error procesando archivos: {str(e)}"), 500
     except Exception as e:
         # Manejar otros errores inesperados
-        return render_template('error.html', error_message=f"An unexpected error occurred: {str(e)}"), 500
-                               
-@app.route('/download/<filename>')
-def download_zip(filename):
-    return send_file(os.path.join(app.config['DOWNLOAD_FOLDER'], filename), as_attachment=True)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=False)
+        return render_template('error.html', error_message=f"An unexpected error occurred: {str(e)}"), 500'''
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,78 +1,100 @@
 function downloadSubtitles() {
     var downloadUrl = $('#downloadLink').attr('href');
     window.location.href = downloadUrl;
-  }
-  
-  $(document).ready(function() {
-    // Objeto para almacenar el progreso de cada archivo
-    var fileUploadProgress = {};
-  
-    // Función para actualizar la barra de progreso de un archivo
-    function updateProgressBar(fileName, percentComplete) {
-      var progress = $(`.progress-bar[data-file-name="${fileName}"]`);
-      var counter = $(`.counter[data-file-name="${fileName}"]`);
-  
-      // Actualiza el texto del contador y el ancho de la barra de progreso
-      counter.text(Math.round(percentComplete) + '%');
-      progress.css('width', percentComplete + '%');
-    }
-  
-    // Ocultar la información de progreso al cargar la página
-    $('.progress-container').hide();
-    $('.counter').hide();
-  
-    // Crear una nueva instancia de EventSource para escuchar eventos SSE
-    var eventSource = new EventSource('/upload');
-  
-    // Manejar eventos SSE recibidos del servidor
-    eventSource.onmessage = function(event) {
-      var data = JSON.parse(event.data);
-      var fileName = data.fileName;
-      var progress = parseInt(data.progress);
-  
-      // Actualizar la barra de progreso del archivo correspondiente
-      updateProgressBar(fileName, progress);
-  
-      // Ocultar la información de progreso si la carga ha finalizado
-      if (progress === 100) {
-        $(`.progress-container[data-file-name="${fileName}"]`).hide();
-      }
-    };
-  
-    // Cerrar la conexión SSE cuando la página se cierre o se recargue
-    $(window).on('beforeunload', function() {
-      eventSource.close();
+}
+// Función para actualizar la barra de progreso y el texto
+function updateProgressBar(percentComplete) {
+    var progressBar = $('#progressBar');
+    var progressText = $('#progressText');
+
+    // Actualiza el valor de la barra de progreso y el texto
+    progressBar.val(percentComplete);
+    progressText.text(percentComplete + '%');
+}
+function updateProgress() {
+    $.ajax({
+        type: 'GET',
+        url: '/progress',  // Ruta en el servidor para obtener el progreso
+        success: function(response) {
+            if (response && response.progress !== undefined) {
+                // Actualizar la barra de progreso en el frontend
+                //console.log('Progreso recibido:', response.progress);
+                updateProgressBar(response.progress);
+                if (response.progress < 100) {
+                    // Si el progreso no ha alcanzado el 100%, sigue comprobando
+                    setTimeout(updateProgress, 2500);  // Revisa cada segundo
+                } else {
+                    // Cuando el progreso alcanza el 100%, oculta la barra de progreso
+                    $('#progressBar').addClass('is-hidden');
+                    $('#progressText').addClass('is-hidden');
+                }
+            }
+        },
+        error: function(xhr, status, error) {
+            // Manejo de errores
+        }
     });
-  
-    // Cuando se envía el formulario
-    $('#uploadForm').submit(function(event) {
-      event.preventDefault();
-      var formData = new FormData($(this)[0]);
-      var progressContainers = $('.progress-container'); // Seleccionar todos los contenedores de progreso
-  
-      // Mostrar la información de progreso de todos los archivos
-      progressContainers.show();
-      $('.counter').show();
-  
-      $.ajax({
+}
+// Cuando se envía el formulario
+$('#uploadForm').submit(function(event) {
+    event.preventDefault();
+
+    // Desactivar el botón de submit
+    $('#submitButton').prop('disabled', true);
+
+    var formData = new FormData($(this)[0]);
+    $('#progressBar').removeClass('is-hidden');
+    $('#progressText').removeClass('is-hidden');
+
+    $.ajax({
         type: 'POST',
         url: '/upload',
         data: formData,
         cache: false,
         contentType: false,
         processData: false,
+        /*xhrFields: {
+            // Agregar esta opción para permitir eventos progresivos desde el servidor
+            onprogress: function(e) {
+                if (e.lengthComputable) {
+                    // Calcular el porcentaje completo
+                    var percentComplete = (e.loaded / e.total) * 100;
+                    // Actualizar la barra de progreso y el texto
+                    updateProgressBar(percentComplete);
+                }
+            }
+        },*/
+        
         success: function(response) {
-          // Ocultar la información de progreso de todos los archivos
-          progressContainers.hide();
-          $('.counter').hide();
-  
-          // Mostrar el botón de descarga del ZIP
-          $('#downloadLink').removeClass('is-hidden').attr('href', response);
+            // Ocultar la información de progreso de todos los archivos
+            
+            $('#progressBar').addClass('is-hidden');
+            $('#progressText').addClass('is-hidden');
+            $('.counter').hide();
+
+            // Mostrar el botón de descarga del ZIP
+            $('#h2_name').removeClass('is-hidden');
+            $('#downloadLink').removeClass('is-hidden').attr('href', response);
+            // activar el botón de submit
+            $('#submitButton').prop('disabled', false);
+            // Detener la llamada a la función updateProgress
+            clearTimeout(updateProgressTimeout);
+            
+            
         },
         error: function(xhr, status, error) {
-          // Mostrar un mensaje de error al usuario
-          alert('Ha ocurrido un error al subir los archivos. Inténtalo de nuevo más tarde.');
+            // Mostrar un mensaje de error al usuario
+            alert('Ha ocurrido un error al subir los archivos. Inténtalo de nuevo más tarde.');
+            // Desactivar el botón de submit
+            $('#submitButton').prop('disabled', false);
         }
-      });
     });
-  });
+    /*function callUpdateProgress() {
+        updateProgress();
+        setTimeout(callUpdateProgress, 1000); // Llama a esta función cada 200 milisegundos
+    }
+    callUpdateProgress();*/
+    var updateProgressTimeout = setTimeout(updateProgress, 2500);
+});
+
+
